12b27cffd71cd4e7d8dc4d87120ad3613343e181
Jeffrey Leung <jleung513@gmail.com>
2019-02-01 20:46:25 -0800

    Hide navbar when at top of page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,6 +10,19 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    // Source: https://www.w3schools.com/howto/howto_js_navbar_slide.asp
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+      var distanceFromTop = 200;
+      if (document.body.scrollTop > distanceFromTop ||
+          document.documentElement.scrollTop > distanceFromTop) {
+        document.getElementById("navbar").style.top = "0";
+      } else {
+        document.getElementById("navbar").style.top = "-100px";
+      }
+    }
   }
 
 }
