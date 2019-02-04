256388ff2d76589dd8ff60055bf4759639da458b
anmolbajajnet <hello@anmolbajaj.net>
2019-02-03 18:49:21 -0800

    Add routes, seperate the code in AppComponent to HomeComponent, enable routes for the About Us page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,12 @@ import { GiveComponent } from './give/give.component';
 import { SeekComponent } from './seek/seek.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about-us', component: AboutComponent}
+];
 
 
 @NgModule({
@@ -20,13 +26,15 @@ import { AboutComponent } from './about/about.component';
     GiveComponent,
     SeekComponent,
     FooterComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
